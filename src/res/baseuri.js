b3ebb35580b23e3ffa5d37c1b158ef7d4bf3688b
eunille <eunille.villegas@growsari.com>
2021-09-17 21:46:17 +0800

    set https://svms-iclear-server.herokuapp.com as the base endpoint

--- src/res/baseuri.js
+++ src/res/baseuri.js
@@ -1,4 +1,4 @@
-const ipAdd = '192.168.125.201';  ///`192.168.100.3` // <-- change this to the ipV4 of your machine
+const ipAdd = 'https://svms-iclear-server.herokuapp.com';  ///`192.168.100.3` // <-- change this to the ipV4 of your machine
 
 const uri = ipAdd;
 const baseuri = `http://${uri}:4040`;
