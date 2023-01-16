5eeb78dec2b61f2b0655a83a47d10cf201412b8e
Eunille <eunille.villegas@growsari.com>
2023-01-16 23:48:28 +0800

    update link

--- src/res/baseuri.js
+++ src/res/baseuri.js
@@ -1,4 +1,4 @@
-const heroku = 'https://svms-iclear-server.herokuapp.com';
+const heroku = 'https://dev-svms-express-server.herokuapp.com';
 const local = 'http://192.168.125.201:4040';
 const baseuri = heroku || local;
 
